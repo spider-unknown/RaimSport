20aa2709d16d3f5aeb2850734da5e52df4108dd2
unknown_haribda <bekza.bekmuratov@gmail.com>
2020-02-17 17:04:42 +0600

    Admin side css js;

---
+++ resources/views/admin/layouts/admin.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('admin.layouts.parts.head');
+</head>
+
+<body class="">
+<div class="wrapper ">
+    <div class="sidebar" data-color="purple" data-background-color="white" data-image="{{asset('admin/img/sidebar-1.jpg')}}">
+        @include('admin.layouts.parts.sidebar')
+    </div>
+    <div class="main-panel">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+            @include('admin.layouts.parts.navbar')
+        </nav>
+        <!-- End Navbar -->
+        <div class="content">
+            @yield('content')
+        </div>
+    </div>
+</div>
+@include('admin.layouts.parts.javascript')
+</body>
+</html>
