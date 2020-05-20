758f5253ab6ee85b051fcea5ad95aabe2cdda486
air <asilkhan-al@mail.ru>
2020-05-21 04:19:24 +0600

    Client side changed

--- resources/views/client/layouts/parts/header.blade.php
+++ resources/views/client/layouts/parts/header.blade.php
@@ -26,6 +26,9 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="fa fa-bars"></span> Menu
             </button>
+            <div class="req-button order-lg-last">
+                <a href="#">Request A Quote</a>
+            </div>
             <div class="collapse navbar-collapse" id="ftco-nav">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active"><a href="{{route('client.index')}}" class="nav-link">Home</a></li>
@@ -39,35 +42,12 @@
         </div>
     </nav>
     <!-- END nav -->
-    <div class="hero-wrap js-fullheight">
-        <div class="home-slider js-fullheight owl-carousel">
-            <div class="slider-item js-fullheight" style="background-image:url({{asset('client/images/bg_1.jpg')}});">
-                <div class="overlay"></div>
-                <div class="container">
-                    <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
-                        <div class="col-md-10 text-center ftco-animate">
-                            <div class="text w-100">
-                                <h2>We are best Roofing Services</h2>
-                                <h1 class="mb-4">Dedicated to providing the highest quality roofing services</h1>
-                                <p><a href="#" class="btn btn-primary">Book an appointment</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="slider-item js-fullheight" style="background-image:url({{asset('client/images/bg_2.jpg')}});">
-                <div class="overlay"></div>
-                <div class="container">
-                    <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
-                        <div class="col-md-8 text-center ftco-animate">
-                            <div class="text w-100">
-                                <h2>We care about your home</h2>
-                                <h1 class="mb-4">Quality roofing at a affordable price</h1>
-                                <p><a href="#" class="btn btn-primary">Book an appointment</a></p>
-                            </div>
-                        </div>
-                    </div>
+    <div class="hero-wrap hero-wrap-2" style="background-image: url({{asset('/client/images/bg_2.jpg')}}); background-position: 50% 0%;" data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row no-gutters slider-text align-items-end">
+                <div class="col-md-9 ftco-animate pb-5 fadeInUp ftco-animated">
+                    {{--<h1 class="mb-0 bread">About Us</h1>--}}
                 </div>
             </div>
         </div>
