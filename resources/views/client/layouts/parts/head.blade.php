5bbbdc688e0599ea87274ebfcb36a0d5052bdbca
air <asilkhan-al@mail.ru>
2020-05-21 16:11:44 +0600

    Raimsport in process

--- resources/views/client/layouts/parts/head.blade.php
+++ resources/views/client/layouts/parts/head.blade.php
@@ -2,6 +2,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+<link rel="icon" type="image/png" href="{{asset('favicon.ico')}}"/>
+
 <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
