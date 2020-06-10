ccb21f9829ae4165ec7a9a5300064b749e5d322b
air <asilkhan-al@mail.ru>
2020-06-10 11:28:00 +0600

    Shop added

--- resources/views/client/branch.blade.php
+++ resources/views/client/branch.blade.php
@@ -1,4 +1,12 @@
 @extends('client.layouts.client')
+
+@section('styles')
+    <style>
+        .info {
+            overflow-wrap: break-word;
+        }
+    </style>
+@endsection
 @section('content')
     <section class="ftco-section">
         <div class="container">
@@ -6,29 +14,30 @@
                 <div class="col-lg-3 col-md-4 col-12">
                     <ul class="list-group">
                         @foreach($categories as $category)
-                                <li class="list-group-item @if($categoryId == $category->id) bg-primary text-white @endif">
-                                    <div>
-                                        <a class="@if($categoryId == $category->id) text-white @endif"
-                                           href="{{route('client.branch', ['categoryId' => $category->id,
+                            <li class="list-group-item @if($categoryId == $category->id) bg-primary text-white @endif">
+                                <div>
+                                    <a class="@if($categoryId == $category->id) text-white @endif"
+                                       href="{{route('client.branch', ['categoryId' => $category->id,
                                        'id' => $branch->id])}}">{{$category->name}}</a>
-                                        @if(!$category->products->isEmpty())
-                                            <a href="#" data-toggle="dropdown">
-                                                <span class="fa fa-caret-down @if($categoryId == $category->id) bg-primary text-white @endif"></span></a>
-                                            <ul class="dropdown-menu">
-                                                @foreach($category->products as $product)
-                                                    <li class="dropdown-item @if($productId == $product->id) bg-primary text-white @endif">
-                                                        <a class="@if($productId == $product->id) text-white @endif"
-                                                            href="{{route('client.branch', ['categoryId' => $category->id,
-                                                        'id' => $branch->id, 'productId' => $product->id])}}">{{$product->name}}</a></li>
-                                                @endforeach
-                                            </ul>
-                                        @endif
-                                    </div>
-                                </li>
+                                    @if(!$category->products->isEmpty())
+                                        <a href="#" data-toggle="dropdown">
+                                            <span class="fa fa-caret-down @if($categoryId == $category->id) bg-primary text-white @endif"></span></a>
+                                        <ul class="dropdown-menu">
+                                            @foreach($category->products as $product)
+                                                <li class="dropdown-item @if($productId == $product->id) bg-primary text-white @endif">
+                                                    <a class="@if($productId == $product->id) text-white @endif"
+                                                       href="{{route('client.branch', ['categoryId' => $category->id,
+                                                        'id' => $branch->id, 'productId' => $product->id])}}">{{$product->name}}</a>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    @endif
+                                </div>
+                            </li>
                         @endforeach
                     </ul>
                 </div>
-                <div class="col-lg-9 col-md-8 col-12">
+                <div class="col-lg-9 col-md-8 col-12 info">
                     <div class="container">
                         @if(!$currentProduct)
                             <div class="col-lg-9 order-lg-last ftco-animate">
@@ -40,29 +49,30 @@
                                     {!! $currentCategory->description !!}
                                 @else
                                     <h2 class="mb-3">{{$branch->name}}</h2>
-                                        {!! $branch->description !!}
+                                    {!! $branch->description !!}
                                 @endif
-                            @if(!empty($products))
-                                <div class="row d-flex">
-                                    @foreach($products as $product)
-                                        <a href="{{route('client.branch', ['categoryId' => $category->id,
+                                @if(!empty($products))
+                                    <div class="row d-flex">
+                                        @foreach($products as $product)
+                                            <a href="{{route('client.branch', ['categoryId' => $category->id,
                                                         'id' => $branch->id, 'productId' => $product->id])}}">
-                                        <div class="col-md-4 mb-4 d-flex ftco-animate">
-                                            <div class="card" style="width: 30rem;">
-                                                <img class="card-img-top" src="{{asset($product->img_path)}}"
-                                                     alt="Card image cap">
-                                                <div class="card-body">
-                                                    <div class="row"><h5 class="card-title">{{$product->name}}</h5>
-                                                        <p class="card-text">{{$product->price}}тг</p>
+                                                <div class="col-md-4 mb-4 d-flex ftco-animate">
+                                                    <div class="card" style="width: 30rem;">
+                                                        <img class="card-img-top" src="{{asset($product->img_path)}}"
+                                                             alt="Card image cap">
+                                                        <div class="card-body">
+                                                            <div class="row"><h5
+                                                                        class="card-title">{{$product->name}}</h5>
+                                                                <p class="card-text">{{$product->price}}тг</p>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </div>
-                                        </a>
-                                    @endforeach
-                                </div>
-                            @endif
-                        </div>
+                                            </a>
+                                        @endforeach
+                                    </div>
+                                @endif
+                            </div>
                         @else
                             <div class="col-lg-9 order-lg-last ftco-animate">
                                 <div class="card">
@@ -82,7 +92,8 @@
                                     <div class="card-footer">
                                         <div class="contact-wrap w-100 p-md-5 p-4">
                                             <h3 class="mb-4">Оставьте заявку, мы вам позвоним</h3>
-                                            <form method="POST" action="{{route('client.bot.send.contact')}}" id="contactForm"
+                                            <form method="POST" action="{{route('client.bot.send.contact')}}"
+                                                  id="contactForm"
                                                   class="contactForm">
                                                 {{csrf_field()}}
                                                 <div class="row">
@@ -96,19 +107,22 @@
                                                     </div>
                                                     <div class="col-md-12">
                                                         <div class="form-group">
-                                                            <input type="text" class="form-control" name="phone" id="subject"
+                                                            <input type="text" class="form-control" name="phone"
+                                                                   id="subject"
                                                                    placeholder="Номер телефона">
                                                         </div>
                                                     </div>
                                                     <div class="col-md-6">
                                                         <div class="form-group">
-                                                            <input type="text" class="form-control" name="name" id="name"
+                                                            <input type="text" class="form-control" name="name"
+                                                                   id="name"
                                                                    placeholder="Имя">
                                                         </div>
                                                     </div>
                                                     <div class="col-md-6">
                                                         <div class="form-group">
-                                                            <input type="email" class="form-control" name="email" id="email"
+                                                            <input type="email" class="form-control" name="email"
+                                                                   id="email"
                                                                    placeholder="Email">
                                                         </div>
                                                     </div>
