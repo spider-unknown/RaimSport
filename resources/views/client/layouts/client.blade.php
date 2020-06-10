642877fff62c6749ef45801ac925f5d0f69676c6
air <asilkhan-al@mail.ru>
2020-06-10 11:29:50 +0600

    Shop added

--- resources/views/client/layouts/client.blade.php
+++ resources/views/client/layouts/client.blade.php
@@ -2,11 +2,13 @@
 <html lang="en">
 <head>
     @include('client.layouts.parts.head')
+    @yield('styles')
 </head>
 <body>
 @include('client.layouts.parts.header')
 @yield('content')
 @include('client.layouts.parts.footer')
 @include('client.layouts.parts.javascript')
+@yield('scripts')
 </body>
 </html>
