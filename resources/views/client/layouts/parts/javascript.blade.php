8d5d9cbb37511c4de2f125f8392aff4961137c6c
daur <daur-lion@mail.ru>
2020-06-11 14:33:57 +0600

    correction

--- resources/views/client/layouts/parts/javascript.blade.php
+++ resources/views/client/layouts/parts/javascript.blade.php
@@ -55,9 +55,9 @@
 
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    };
+    // span.onclick = function() {
+    //     modal.style.display = "none";
+    // };
 
     // When the user clicks anywhere outside of the modal, close it
 
